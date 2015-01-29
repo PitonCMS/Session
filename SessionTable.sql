226a4919c8df6c339d9dd190026cf67f7849829b
Wolfgang Moritz <wolfmoritz@gmail.com>
2015-01-28 22:10:43 -0800

    Added session table definition

---
+++ SessionTable.sql
@@ -0,0 +1,15 @@
+--
+-- Create Session Table
+--
+-- If you change the name of the table be sure to provide the new
+-- name as a configuation item for $config['tableName']
+--
+
+CREATE TABLE IF NOT EXISTS `session` (
+  `session_id` char(64) NOT NULL,
+  `data` text,
+  `user_agent` char(64) DEFAULT NULL,
+  `ip_address` varchar(46) DEFAULT NULL,
+  `time_updated` int(11) DEFAULT NULL,
+  PRIMARY KEY (`session_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;

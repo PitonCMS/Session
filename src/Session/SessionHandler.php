0f3d9bd8159296327f2c256bfe6e0f97ebc34067
Wolfgang Moritz <wolfmoritz@gmail.com>
2015-01-29 20:42:29 -0800

    Modified namespace and autoloading path

--- src/Session/SessionHandler.php
+++ src/Session/SessionHandler.php
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
 */
-namespace WolfMoritz;
+namespace WolfMoritz\Session;
 
 use \PDO;
 use \Exception;
